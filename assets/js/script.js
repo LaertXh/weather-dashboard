bc5f64b6ec2656a64bb25ee3d3c7355dbe0e3f78
Laert Xhumari <laertxhumari@gmail.com>
2023-02-21 22:41:22 -0500

    todays information get updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 //DEPENDENCIES ==================================================================================
-let userInputTextEl = document.querySelector('.input');
-let userInputButtonEl = document.querySelector('#submit-button');
+let userInputTextEl = document.querySelector(".input");
+let userInputButtonEl = document.querySelector("#submit-button");
+let cityNameEl = document.querySelector("#city-name");
+let tempTextEl = document.querySelector("#temp-text");
+let windTextEl = document.querySelector("#wind-text");
+let humidityTextEl = document.querySelector("#humidity-text");
 
 //DATA ==========================================================================================
 let apiKey = "37cf2af51487d6436e871e44f1db13c6";
@@ -17,6 +21,19 @@ function submitButtonListener(event){
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
+
+        //update data for today's date 
+        let cityName = data.city.name;
+        let temp =  ((data.list[0].main.temp) - 273.15) * 9/5 + 32;
+        let wind = data.list[0].wind.speed;
+        let humidity = data.list[0].main.humidity;
+
+        cityNameEl.innerHTML = cityName;
+        tempTextEl.innerHTML = "Temp: " + Math.round(temp * 100) / 100 + '°F';
+        windTextEl.innerHTML = "Wind: " + wind + " MPH";
+        humidityTextEl.innerHTML = "Humidity: " + humidity + "%";
+
+
     });
 }
 
