c9612aa1524e6a6b1040eba7b47faea6f4bea29d
Laert Xhumari <laertxhumari@gmail.com>
2023-02-28 17:28:29 -0500

    added styles for the weather panels. Created html structure of pannels

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 //DEPENDENCIES ==================================================================================
+let formEl = document.querySelector("#form");
 let userInputTextEl = document.querySelector(".input");
 let userInputButtonEl = document.querySelector("#submit-button");
 let cityNameEl = document.querySelector("#city-name");
@@ -7,7 +8,7 @@ let windTextEl = document.querySelector("#wind-text");
 let humidityTextEl = document.querySelector("#humidity-text");
 
 //DATA ==========================================================================================
-let apiKey = "37cf2af51487d6436e871e44f1db13c6";
+let apiKey = "4dcc04aad0e12d160288086e453dc2ff";
 let city;
 //FUNCTIONS =====================================================================================
 
@@ -22,8 +23,11 @@ function submitButtonListener(event){
     .then((data) => {
         console.log(data);
 
+        //date format fix
+        let reformatDate = rewriteDate(data.list[0].dt_txt.split("-"));
+
         //update data for today's date 
-        let cityName = data.city.name;
+        let cityName = data.city.name + " (" + reformatDate + ")";
         let temp =  ((data.list[0].main.temp) - 273.15) * 9/5 + 32;
         let wind = data.list[0].wind.speed;
         let humidity = data.list[0].main.humidity;
@@ -33,13 +37,34 @@ function submitButtonListener(event){
         windTextEl.innerHTML = "Wind: " + wind + " MPH";
         humidityTextEl.innerHTML = "Humidity: " + humidity + "%";
 
+        //update data for the next 5 days
+        for(let i = 7; i < 40; i += 8){
+            let elem = data.list[i];
+            let htmlText = 
+                           `<div class="panel">
+                                <h3 class="date">${rewriteDate(elem.dt_txt.split("-"))}</h3>
+                                <img src="https://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png" alt="weather-icon">
+                                <p>Temp: ${((elem.main.temp) - 273.15) * 9/5 + 32}'°F'</p>
+                                <p>Wind: ${elem.wind.speed} MPH</p>
+                                <p>Humidity: ${elem.main.humidity}%</p> 
+                            </div>`
+        }
+
 
     });
 }
 
+//given a date from the weather api, reformat it mm/dd/yyyy
+function rewriteDate(reformatDate){
+    reformatDate[2] = reformatDate[2].split(" ");
+    reformatDate[2] = reformatDate[2][0];
+    reformatDate = reformatDate[1] + "/" + reformatDate[2] +"/" + reformatDate[0];
+    return reformatDate;
+}
+
 //USER INTERACTIONS =============================================================================
 //when user clicks submit then something happens
-userInputButtonEl.addEventListener('click', submitButtonListener);
+formEl.addEventListener('submit', submitButtonListener);
 
 
 //INITIALIZATIONS ===============================================================================
